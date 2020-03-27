c10257bac8bf1fc94fee453787d1dc3acc3428b0
Grigori Jlavyan <jlavyan@gmail.com>
2020-03-28 02:08:29 +0400

    ADD test cases

--- VideoTests/VideoTests.swift
+++ VideoTests/VideoTests.swift
@@ -7,22 +7,53 @@
 //
 
 import XCTest
+import Alamofire
+import RxSwift
+
 @testable import Video
 
 class VideoTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testLoadingData() {
+        let disposeBag = DisposeBag()
+
+        VideoRepository().videoList().observeOn(MainScheduler.instance)
+        .subscribe(onNext: { vs in
+            if(vs.count == 0){
+                XCTAssertEqual(vs.count == 0, true, "Error on load data via internet")
+            }
+        }, onError: { error in
+            XCTAssertEqual(true, true, "Test passes with error \(error)")
+        })
+        .disposed(by: disposeBag)
     }
+    
+    
+    func testDownloadFile() {
+        let disposeBag = DisposeBag()
+
+        let downloader = Downloader()
+        let dictionary: [String: Any] = ["id": 1, "name": "hey", "thumbnail": "asd",
+                                         "description": "any description", "video_link": "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"]
+        let video = Video(dictionary: dictionary)
+        
+        downloader.downloadData(video: video).observeOn(MainScheduler.instance)
+        .subscribe(onNext: { vs in
+            XCTAssertEqual(vs.count == 0, true, "Error on download hls")
+        }, onError: { error in
+            XCTAssertEqual(true, true, "Test passes with error \(error)")
+        })
+        .disposed(by: disposeBag)
+    }
+
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
